a6d54e906590b5271ebf1c59b076b8c5bffe1d9f
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2013-01-08 15:25:04 +0100

    rewriting to handle custom methods

--- madame/dispatcher.py
+++ madame/dispatcher.py
@@ -1,14 +1,22 @@
 from flask.views import MethodView
 
+#: TODO root_handler, collection_handler, item_handler
+
 class Dispatcher(MethodView):
     def __init__(self, app):
         self.app = app
 
     def get(self, collection=None, id=None):
+        #:
+
+        #: /collection/id
         if id:
             return 'Id'
+        #: /collection/
         elif collection:
             return 'Collection'
+        #: /
         else:
+
             return 'Root'
 
